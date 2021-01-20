458a62a46b02395a9524725b0440ac82c73e6ae5
EnioAmarantes <enio.amarantes@gmail.com>
2021-01-20 08:21:18 -0300

    Geração do Projeto base

---
+++ Source/AppSocial/AppSocial/AppSocial/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppSocial.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppSocial.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Source/AppSocial/AppSocial/AppSocial/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppSocial.Models;
+using AppSocial.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppSocial.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
